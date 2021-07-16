7a5619d083ef19107f8746e64e1b2d5f4828e8f0
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-15 18:54:14 -0600

    creates response in desired formatting bexcpet it includes the step_id in the ingredients

--- api/recipes-model.js
+++ api/recipes-model.js
@@ -18,73 +18,68 @@ const getRecipeById = async id => {
     // on i.ingredient_id = si.ingredient_id
     // where r.recipe_id = 2;
  
-    const unstructuredRecipe = await db
-        .select(
-            'r.*', 
-            's.step_id', 
-            's.step_number', 
-            's.step_instructions',
-            'i.ingredient_id',
-            'i.ingredient_name',
-            'si.ingredient_quantity')
-        .from('recipes as r')
-        .join('steps as s', 's.recipe_id', 'r.recipe_id')
-        .join('steps_ingredients as si', 'si.step_id', 's.step_id')
-        .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
-        .where('r.recipe_id', id)
-        .orderBy('s.step_number')
-    console.log(unstructuredRecipe)
+const unstructuredRecipe = await db
+    .select(
+        'r.*', 
+        's.step_id', 
+        's.step_number', 
+        's.step_instructions',
+        'i.ingredient_id',
+        'i.ingredient_name',
+        'si.ingredient_quantity')
+    .from('recipes as r')
+    .join('steps as s', 's.recipe_id', 'r.recipe_id')
+    .join('steps_ingredients as si', 'si.step_id', 's.step_id')
+    .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
+    .where('r.recipe_id', id)
+    .orderBy('s.step_number')
 
-    const ingredients = unstructuredRecipe.map(item => {
-        
-            if(!item["ingredient_id"]){
-                return []
-        } else {
-            return {
-                "step_id": item["step_id"],
-                "ingredient_id": item["ingredient_id"],
-                "ingredient_name": item["ingredient_name"],
-                "quantity": item["ingredient_quantity"]
-            }
-        }
-    })
+const ingredients = unstructuredRecipe.map(item => {
     
-    // const stepIngredients = ingredients.filter((ingredient) => {
-    //     return ingredient["step_id"] === item["step_id"]
-    // })
-
-    // console.log("test filtering", stepIngredients)
-    
-    const steps = unstructuredRecipe.map(item => {
-        const stepId = item["step_id"]
+        if(!item["ingredient_id"]){
+            return []
+    } else {
         return {
             "step_id": item["step_id"],
-            "step_number": item["step_number"],
-            "step_instructions": item["step_instructions"],
-            "ingredients": ingredients.filter((ingredient) => {
-                return ingredient["step_id"] === stepId
-            })
+            "ingredient_id": item["ingredient_id"],
+            "ingredient_name": item["ingredient_name"],
+            "quantity": item["ingredient_quantity"]
         }
-    })
-    
-    // console.log(steps)
-    
-    const recipe = {
-        "recipe_id": unstructuredRecipe[0]["recipe_id"],
-        "recipe_name": unstructuredRecipe[0]["recipe_name"],
-        "created_at": unstructuredRecipe[0]["created_at"],
-        "steps": steps
     }
-    
-    console.log("start of recipe:", recipe)
+})
 
-    return recipe
+const steps = unstructuredRecipe.map(item => {
+    const stepId = item["step_id"]
+    return {
+        "step_id": item["step_id"],
+        "step_number": item["step_number"],
+        "step_instructions": item["step_instructions"],
+        "ingredients": ingredients.filter((ingredient) => {
+            return ingredient["step_id"] === stepId
+        })
+    }
+})
 
-    // const ingredients = unstructuredRecipe.reducer((item, acc) => {
-    //     return {
+const repeatStepsRecipe = {
+    "recipe_id": unstructuredRecipe[0]["recipe_id"],
+    "recipe_name": unstructuredRecipe[0]["recipe_name"],
+    "created_at": unstructuredRecipe[0]["created_at"],
+    "steps": steps
+}
 
-    //     }
-    // }, [])
+const filteredSteps = repeatStepsRecipe.steps.filter((step, index, steps) => 
+    index === steps.findIndex((s) => (
+        s.step_id === step.step_id
+    )))
+
+const recipe = {
+    "recipe_id": unstructuredRecipe[0]["recipe_id"],
+    "recipe_name": unstructuredRecipe[0]["recipe_name"],
+    "created_at": unstructuredRecipe[0]["created_at"],
+    "steps": filteredSteps
+}
+
+return recipe
 }
 
 module.exports = {
